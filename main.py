daccdc852138aa897ae43866ead4707997ef9662
ehabee <ebender2025@kehillahstudent.org>
2025-01-16 09:44:34 -0800

    done

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 def choose_practicum():
    choice = input("sign up for a practicum (lighting, sound, scenery, or costumes): ")
    if choice == "lighting" or choice == "sound" or choice == "scenery" or choice == "costumes":
-      return choice
+    return choice
+   #returns choice
    else:
-      return choose_practicum()
+    return choose_practicum()
+   #run the function choose practium and returns the result
    
 name = input("what is your name? ")
 practicum = choose_practicum()
